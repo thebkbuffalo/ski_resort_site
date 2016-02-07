89a9f8e900f37ac832690a4971ccda9064db4e36
Evan Berg <evan.m.berg@gmail.com>
2016-02-06 18:55:07 -0800

    logic for moving thru questions matrix is good to go...yo.

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
+
+  $('.start').on('click', function(){
+    $('.home').removeClass('active');
+    $('.home').addClass('inactive');
+    $('.travel_with').removeClass('inactive');
+    $('.travel_with').addClass('active');
+  });
+
+  $('.friends').on('click', function(){
+    $('.travel_with').removeClass('active');
+    $('.travel_with').addClass('inactive');
+    $('.friends_experience').removeClass('inactive');
+    $('.friends_experience').addClass('active');
+  });
+
+  $('.family').on('click', function(){
+    $('.travel_with').removeClass('active');
+    $('.travel_with').addClass('inactive');
+    $('.family_experience').removeClass('inactive');
+    $('.family_experience').addClass('active');
+  });
+
+  $('.so').on('click', function(){
+    $('.travel_with').removeClass('active');
+    $('.travel_with').addClass('inactive');
+    $('.so_experience').removeClass('inactive');
+    $('.so_experience').addClass('active');
+  });
+
   
 });
